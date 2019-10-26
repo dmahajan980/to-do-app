1cb4fde8b03728c03d4c16f78c929c852db54647
dmahajan980 <divyanshumahajan98@gmail.com>
2019-10-27 00:43:56 +0530

    Updates

---
+++ precache-manifest.14e63b844ca04fbd416afefcec80a34a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c99a36bce67646552b60409c7b27eb14",
+    "url": "/react-to-do-app/index.html"
+  },
+  {
+    "revision": "603017d6fff84deca721",
+    "url": "/react-to-do-app/static/css/main.cbf7a7fe.chunk.css"
+  },
+  {
+    "revision": "b81fb0b5c89a298ecf6a",
+    "url": "/react-to-do-app/static/js/2.0fd4dac0.chunk.js"
+  },
+  {
+    "revision": "603017d6fff84deca721",
+    "url": "/react-to-do-app/static/js/main.60dd8be1.chunk.js"
+  },
+  {
+    "revision": "8f5a5a2d130a7847b6c8",
+    "url": "/react-to-do-app/static/js/runtime-main.7fbec401.js"
+  }
+]);
